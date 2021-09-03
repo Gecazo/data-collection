ec2d77e87ee5df10ae5fb9b55a864db2d38d708e
Filip <43730405+Gecazo@users.noreply.github.com>
2021-09-03 11:36:12 +0300

    Displaying data on temperature graph

--- sitka_hights.py
+++ sitka_hights.py
@@ -1,4 +1,5 @@
 import csv 
+import matplotlib.pyplot as plt
 
 filename = 'data/sitka_weather_07-2014.csv'
 with open(filename) as f:
@@ -9,3 +10,13 @@ with open(filename) as f:
     
     print(highs)
 
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+ax.set_title("Highest temperature of the day", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
